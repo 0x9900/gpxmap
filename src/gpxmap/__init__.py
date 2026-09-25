19512211d21a537704e2d8dc09dfc4cc82bb7006
Fred C <github-fred@hidzz.com>
2026-09-25 08:55:16 +0200

    Exporting some of the functions and classes

--- src/gpxmap/__init__.py
+++ src/gpxmap/__init__.py
@@ -1,3 +1,13 @@
 
 
-__version__ = '0.1.3'
+from .gpxmap import (DataError, MapPoints, draw_bearings, draw_points,
+                     draw_routes, draw_sail, draw_tacks, draw_waypoints,
+                     export_kml, fly_stats, read_gpx, sail_stats, trek_stats,
+                     type_time)
+
+__version__ = '0.1.6'
+
+__all__ = ["DataError", "MapPoints", "draw_bearings", "draw_points",
+           "draw_routes", "draw_sail", "draw_tacks", "draw_waypoints",
+           "export_kml", "fly_stats", "read_gpx", "sail_stats", "trek_stats",
+           "type_time"]
